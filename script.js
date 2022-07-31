76d167f979f555af39d0f37723f64bd6099f99ee
naimur <naimur202163@gmail.com>
2022-07-31 15:26:24 +0600

    toggle menu is added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello")
+
+const toggleButton=document.getElementsByClassName('navbar-toggle')[0];
+
+const navbarLinks=document.getElementsByClassName('navbar-links');
+
+
+
+
+toggleButton.addEventListener('click',()=>{
+    for(let i=0;i<navbarLinks.length;i++)
+    navbarLinks[i].classList.toggle('active')
+})
