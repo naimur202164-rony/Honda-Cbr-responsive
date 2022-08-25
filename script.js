a6d823d4ae26c3fe4cfe49f40bad07f65b086830
Your Name <naimur202163@gmail.com>
2022-08-25 09:33:23 +0600

    section done

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("Javascript Add");
-const close=document.querySelector('.close');
-const toggle=document.querySelector('.toogle')
-const navbar=document.querySelector('.mobile-nav ul');
+let close=document.querySelector('.close');
+let toggle=document.querySelector('.toogle')
+let navbar=document.querySelector('.mobile-nav ul');
 console.log(navbar)
 console.log(toggle)
 
 toggle.addEventListener('click',()=>{
-    
+
     navbar.style.display="flex";
     // navbar.style.display="none"
     console.log("Clicked")
