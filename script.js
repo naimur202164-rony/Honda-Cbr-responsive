8689d99fca55ea0b93748108af5ada21843f88e8
Your Name <naimur202163@gmail.com>
2022-08-25 09:27:03 +0600

    toggle button added

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-// console.log("Javascript Add");
-// const close=document.querySelector('.close');
-// const toggle=document.querySelector('.toogle')
-// const navbar=document.querySelector('.mobile-nav ul');
-// console.log(navbar)
-// console.log(toggle)
+console.log("Javascript Add");
+const close=document.querySelector('.close');
+const toggle=document.querySelector('.toogle')
+const navbar=document.querySelector('.mobile-nav ul');
+console.log(navbar)
+console.log(toggle)
 
-// toggle.addEventListener('click',()=>{
-//     navbar.style.display="block"
-//     console.log("Clicked")
-// })
+toggle.addEventListener('click',()=>{
+    
+    navbar.style.display="flex";
+    // navbar.style.display="none"
+    console.log("Clicked")
+});
 
 
-// console.log(close,toggle)
+
+
+
+console.log(close,toggle)
