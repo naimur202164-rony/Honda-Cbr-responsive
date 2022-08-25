3d584b198e8fb19bf38a669993ff16963e7c0aec
Your Name <naimur202163@gmail.com>
2022-08-25 09:20:16 +0600

    navbar added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Javascript Add");
+// console.log("Javascript Add");
+// const close=document.querySelector('.close');
+// const toggle=document.querySelector('.toogle')
+// const navbar=document.querySelector('.mobile-nav ul');
+// console.log(navbar)
+// console.log(toggle)
+
+// toggle.addEventListener('click',()=>{
+//     navbar.style.display="block"
+//     console.log("Clicked")
+// })
+
+
+// console.log(close,toggle)
